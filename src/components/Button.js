ec35409ef8e456b2c8b15d437100da16ff3c3261
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-12 15:11:32 -0400

    Implement styling, click handling, and disabled state for Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  let buttonClass = {
+    'button--confirm': props.confirm,
+    'button--danger': props.danger
+  };
+  
+  let disabled = props.disabled ? true : false;
+ 
+  return (
+  <button onClick={props.onClick} disabled={disabled} className={classNames("button", buttonClass)}>
+    {props.children}
+  </button>);
 }
