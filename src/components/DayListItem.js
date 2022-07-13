1813827655501c6cd901cac12d8e5934618702fb
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-12 20:01:30 -0400

    Update state of application on click

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -17,7 +17,7 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={classNames(dayClass)} onClick={() => props.setDay(props.day)}>
+    <li className={classNames(dayClass)} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
