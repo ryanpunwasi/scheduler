78b7e6b9ace89d08aa43de820b632ab2d8b340e6
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-13 15:56:19 -0400

    Implement rendering of InterviewListItems in InterviewList

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import InterviewerListItem from './InterviewerListItem';
+
+import './InterviewerList.scss';
+
+const InterviewerList = (props) => {
+  const { value, onChange } = props;
+  const interviewerItems = props.interviewers.map((interviewer) => {
+    return <InterviewerListItem id={interviewer.id} avatar={interviewer.avatar} name={interviewer.name} setInterviewer={() => onChange(interviewer.id)} selected={interviewer.id === value}/>
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+        {interviewerItems}
+      </ul>
+    </section>
+  );
+};
+
+export default InterviewerList;
