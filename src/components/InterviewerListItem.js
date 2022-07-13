d26b3ed67df7cd26b1ef7078753ca86241f70029
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-13 01:39:22 -0400

    Implement dynamic classes, click handler, and dynamic information for InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './InterviewerListItem.scss';
+
+import classNames from 'classnames';
+
+const InterviewerListItem = (props) => {
+
+  const listItemClass = {
+    "interviewers__item": !props.selected,
+    "interviewers__item--selected": props.selected
+  };
+
+  return (
+    <li key={props.id} className={classNames(listItemClass)} onClick={() => props.setInterviewer(props.id)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+};
+
+export default InterviewerListItem;
