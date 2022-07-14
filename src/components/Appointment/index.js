2e587e82ea25f38c71aaf75334d595cc74cc2e78
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-14 02:02:22 -0400

    Add hard-coded appointments data, render Header and conditionally render Show or Empty in Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React from 'react'
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 const Appointment = (props) => {
+  const { id, time, interview } = props;
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty />}
+    </article>
   );
 };
 
