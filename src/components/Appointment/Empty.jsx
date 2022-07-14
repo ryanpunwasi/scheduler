5b91aa7190d2dc2e53b66f5f20d92c52838d152f
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-14 01:29:21 -0400

    Implement Appointment components

---
+++ src/components/Appointment/Empty.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './styles.scss';
+
+const Empty = props => {
+  return (
+    <main className="appointment__add" onClick={props.onAdd}>
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+      />
+    </main>
+  );
+};
+
+export default Empty;
---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+
+import './styles.scss';
+
+const Form = props => {
+
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent("");
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+  
+  return (
+    <main className="appointment__card appointment__card--create">
+    <section className="appointment__card-left">
+      <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+        <input
+          onChange={(e) => setStudent(e.target.value)}
+          value={student}
+          className="appointment__create-input text--semi-bold"
+          name="name"
+          type="text"
+          placeholder="Enter Student Name"
+        />
+      </form>
+      <InterviewerList 
+      value={interviewer}
+      onChange={setInterviewer}
+      interviewers={props.interviewers}
+      />
+    </section>
+    <section className="appointment__card-right">
+      <section className="appointment__actions">
+        <Button danger onClick={cancel}>Cancel</Button>
+        <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+      </section>
+    </section>
+</main>
+  );
+};
+
+export default Form;
