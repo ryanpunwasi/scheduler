e7878bde129ab29ada346e5d932c7cd7561509f7
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-14 17:29:26 -0400

    Fetch days from scheduler api and pass data to Daylist as prop

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -43,27 +44,16 @@ const appointments = {
   }
 };
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
 
   const [day, setDay] = useState('Monday');
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/days")
+    .then((response) => setDays(response.data));
+  }, []);
+
   const renderedAppointments = Object.values(appointments).map(appointment => {
     return <Appointment key={appointment.id} {...appointment}/>
   });
