99197c648a36625a3f2e13def41879afbc5ee442
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-15 00:03:10 -0400

    Implement selector function to get interview from state

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,3 +8,16 @@ export function getAppointmentsForDay(state, day) {
   let filteredAppointments = filteredDay[0].appointments.map(appointmentId => state.appointments[appointmentId]);
   return filteredAppointments;
 }
+
+export function getInterview(state, interview) {
+
+  const { interviewer } = interview || {};
+
+  if (interviewer in state.interviewers) {
+    const interviewerObj = state.interviewers[interviewer];
+    console.log({ ...interview, interviewer: interviewerObj });
+    return { ...interview, interviewer: interviewerObj };
+  }
+
+  return null;
+}
