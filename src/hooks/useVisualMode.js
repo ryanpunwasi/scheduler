a5dc915ce4299fd5040c78c52ae18a5d76933c2c
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-19 13:54:04 -0400

    Refactor transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode, replace = false) => {  
-    setMode(newMode);
-
-    const newHistory = [...history];
-
+  const transition = (newMode, replace = false) => {
     if (replace) {
-      newHistory.pop();
+      return setMode(newMode);
     }
-    
-    newHistory.push(newMode);
-    setHistory(newHistory);
+
+    setMode(newMode);
+    setHistory([...history, newMode]);
+
+    // const newHistory = [...history];
+
+    // if (replace) {
+    //   newHistory.pop();
+    // }
+
+    // newHistory.push(newMode);
+    // setHistory(newHistory);
   };
 
   const back = () => {
@@ -24,8 +29,7 @@ export default function useVisualMode(initial) {
       setMode(newHistory.at(-1));
       setHistory(newHistory);
     }
-  }
+  };
 
   return { mode, transition, back };
-};
-
+}
