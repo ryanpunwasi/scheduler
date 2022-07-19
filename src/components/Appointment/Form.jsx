7ab1f7d4b2ca23e53eb326928b5cf8356dcde3d3
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-19 13:57:59 -0400

    Change formatting

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import InterviewerList from 'components/InterviewerList';
-import Button from 'components/Button';
+import React, { useState } from "react";
+import InterviewerList from "components/InterviewerList";
+import Button from "components/Button";
 
-import './styles.scss';
+import "./styles.scss";
 
 const Form = props => {
-
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
@@ -18,33 +17,37 @@ const Form = props => {
     reset();
     props.onCancel();
   };
-  
+
   return (
     <main className="appointment__card appointment__card--create">
-    <section className="appointment__card-left">
-      <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
-        <input
-          onChange={(e) => setStudent(e.target.value)}
-          value={student}
-          className="appointment__create-input text--semi-bold"
-          name="name"
-          type="text"
-          placeholder="Enter Student Name"
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            onChange={e => setStudent(e.target.value)}
+            value={student}
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+          />
+        </form>
+        <InterviewerList
+          value={interviewer}
+          onChange={setInterviewer}
+          interviewers={props.interviewers}
         />
-      </form>
-      <InterviewerList 
-      value={interviewer}
-      onChange={setInterviewer}
-      interviewers={props.interviewers}
-      />
-    </section>
-    <section className="appointment__card-right">
-      <section className="appointment__actions">
-        <Button danger onClick={cancel}>Cancel</Button>
-        <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
       </section>
-    </section>
-</main>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
   );
 };
 
