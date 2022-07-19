b2ca0f685df83d8d4e01f39513d3581ad462ffba
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-19 13:56:24 -0400

    Implement prop type checking, do not render if interviewers prop is not an array

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,22 +1,40 @@
-import React from 'react';
-import InterviewerListItem from './InterviewerListItem';
+import React from "react";
+import PropTypes from "prop-types";
 
-import './InterviewerList.scss';
+import InterviewerListItem from "./InterviewerListItem";
 
-const InterviewerList = (props) => {
+import "./InterviewerList.scss";
+
+const InterviewerList = props => {
   const { value, onChange } = props;
-  const interviewerItems = props.interviewers.map((interviewer) => {
-    return <InterviewerListItem key={interviewer.id} id={interviewer.id} avatar={interviewer.avatar} name={interviewer.name} setInterviewer={() => onChange(interviewer.id)} selected={interviewer.id === value}/>
+
+  if (!Array.isArray(props.interviewers)) {
+    return null;
+  }
+
+  const interviewerItems = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        id={interviewer.id}
+        avatar={interviewer.avatar}
+        name={interviewer.name}
+        setInterviewer={() => onChange(interviewer.id)}
+        selected={interviewer.id === value}
+      />
+    );
   });
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list">
-        {interviewerItems}
-      </ul>
+      <ul className="interviewers__list">{interviewerItems}</ul>
     </section>
   );
+};
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
 };
 
 export default InterviewerList;
