c8fbe3e3eea0251530ded77c2c1d94252df9cd84
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-20 17:15:10 -0400

    Revise import statement

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import Appointment from "components/Appointment/index";
+import Appointment from "components/Appointment/";
 
 describe("Appointment", () => {
   it(" renders without crashing", () => {
