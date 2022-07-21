40ee68a2eadeed860bcf0c9e6e0ba8ceb442d681
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-20 21:54:01 -0400

    Add comments and ensure objects in state aren't being referenced directly

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,21 +1,23 @@
 export function getAppointmentsForDay(state, day) {
-  const filteredDay = state.days.filter(d => d.name === day);
-
+  const filteredDay = state.days.filter(d => d.name === day); // Get array containing day object
   if (!filteredDay.length) {
     return [];
   }
 
-  let filteredAppointments = filteredDay[0].appointments.map(
-    appointmentId => state.appointments[appointmentId]
-  );
+  // Create an array of appointment objects for given day
+  let filteredAppointments = filteredDay[0].appointments.map(appointmentId => {
+    return { ...state.appointments[appointmentId] };
+  });
+
   return filteredAppointments;
 }
 
 export function getInterview(state, interview) {
   const { interviewer } = interview || {};
 
+  // Create interview object, where the interviewer property is and object containing interviewer details
   if (interviewer in state.interviewers) {
-    const interviewerObj = state.interviewers[interviewer];
+    const interviewerObj = { ...state.interviewers[interviewer] };
     return { ...interview, interviewer: interviewerObj };
   }
 
@@ -24,27 +26,30 @@ export function getInterview(state, interview) {
 
 export const getInterviewersForDay = (state, day) => {
   const filteredDay = state.days.filter(d => d.name === day);
-
   if (!filteredDay.length) {
     return [];
   }
 
-  let interviewers = filteredDay[0].interviewers.map(
-    interviewerId => state.interviewers[interviewerId]
-  );
+  // Create an array of interviewer objects for given day
+  let interviewers = filteredDay[0].interviewers.map(interviewerId => {
+    return { ...state.interviewers[interviewerId] };
+  });
+
   return interviewers;
 };
 
 export const updateSpots = state => {
+  // Create day object based on selected day
   const currentDayIndex = state.days.findIndex(day => day.name === state.day);
-  const currentDay = state.days[currentDayIndex];
+  const currentDay = { ...state.days[currentDayIndex] };
+
+  // Calculate spots remaining
   const spots = currentDay.appointments.filter(
     id => !state.appointments[id].interview
   ).length;
 
-  const updatedDayObject = { ...currentDay, spots };
-  const updatedDays = [...state.days];
-  updatedDays[currentDayIndex] = updatedDayObject;
+  const updatedDays = [...state.days]; // Get copy of days array from state
+  updatedDays[currentDayIndex] = { ...currentDay, spots }; // Update spots in appropriate day object
 
   return { ...state, days: updatedDays };
 };
