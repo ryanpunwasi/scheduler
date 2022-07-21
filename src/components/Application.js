627faf13783d3ab1cba9725fa6fa863408b5f24e
Ryan Punwasi <ryan.punwasi@gmail.com>
2022-07-20 21:55:57 -0400

    Adjust formatting

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,43 +3,57 @@ import React from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-import useApplicationData from '../hooks/useApplicationData';
+import useApplicationData from "../hooks/useApplicationData";
 
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
 
 import "components/Application.scss";
 
-export default function Application(props) {
-
-  const {
-    state,
-    setDay,
-    bookInterview,
-    cancelInterview
-  } = useApplicationData();
+export default function Application() {
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
 
-  const renderedAppointments = Object.values(dailyAppointments).map(appointment => {
-    const interview = getInterview(state, appointment.interview);
-    
-    return <Appointment key={appointment.id} {...appointment} interview={interview} interviewers={interviewers} bookInterview={bookInterview} cancelInterview={cancelInterview}/>
-  });
+  const renderedAppointments = Object.values(dailyAppointments).map(
+    appointment => {
+      const interview = getInterview(state, appointment.interview);
+
+      return (
+        <Appointment
+          key={appointment.id}
+          {...appointment}
+          interview={interview}
+          interviewers={interviewers}
+          bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
+        />
+      );
+    }
+  );
 
   return (
     <main className="layout">
       <section className="sidebar">
-        <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler"/>
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          value={state.day}
-          onChange={setDay}
-        />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
-        <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs"/>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {renderedAppointments}
